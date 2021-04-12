9d8d91c85d9511776084ee967a2fd185dcda64de
Betül Karagöz <betul.karagoz@technarts.com>
2021-04-12 12:52:04 +0300

    minor bug fix

--- backend/src/main/java/com/bbm384/badgateway/repository/SubClubRepository.java
+++ backend/src/main/java/com/bbm384/badgateway/repository/SubClubRepository.java
@@ -1,17 +1,17 @@
 package com.bbm384.badgateway.repository;
 
+import com.bbm384.badgateway.model.Club;
 import com.bbm384.badgateway.model.SubClub;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface SubClubRepository extends JpaRepository<SubClub, Long> {
+public interface SubClubRepository extends JpaRepository<SubClub, Long> , QuerydslPredicateExecutor<SubClub> {
     Optional<SubClub> findById(Long id);
     Optional<SubClub> findByName(String name);
-
-    Page<SubClub> findAll(BooleanExpression query, Pageable pageable);
 }
