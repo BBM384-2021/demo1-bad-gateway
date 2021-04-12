8a84ab29ad48e702cdc1fcf2e91651925674a499
Salih Kerem Harman <keremharman66@gmail.com>
2021-04-12 23:33:34 +0300

    login page revised

--- frontend/src/components/Layout/index.js
+++ frontend/src/components/Layout/index.js
@@ -3,10 +3,16 @@ import FooterComponent from "./Footer";
 import HeaderComponent from "./Header";
 import { Layout, Menu, Breadcrumb } from 'antd';
 const LayoutComponent = ({header=true,children}) => {
+	const height = window.innerHeight;
 	return (
 		<Layout>
 			{header && <HeaderComponent/>}
-			<main>
+			<main style={{
+				padding: '0 150px',
+				marginTop: 80,
+				overflow: 'initial',
+				height:height
+			}}>
 				{children}
 			</main>
 			<FooterComponent />
--- frontend/src/pages/Auth/Login.js
+++ frontend/src/pages/Auth/Login.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Layout, Form, Input, Button, Checkbox } from 'antd';
+import { Layout, Form, Input, Button, Checkbox,Row, Col, Divider } from 'antd';
 import {LoadingOutlined} from "@ant-design/icons";
 import { useForm, Controller } from 'react-hook-form';
 import {useAuthContext} from "../../context/Auth";
@@ -8,70 +8,61 @@ const { Content } = Layout;
 const Login = () => {
   const { control, register, handleSubmit, setValue } = useForm();
 	const {userLogin,authState} = useAuthContext();
-  // useEffect(()=>{
-  // 	register({username:"username"},{required:true});
-  // 	register({password:"password"},{required:true});
-  // },[])
 
   const onSubmit = (data) => {
     userLogin(data);
   };
   return (
-    <Content
-      style={{
-        padding: '0 150px',
-        minHeight: '100%',
-        marginTop: 64,
-        overflow: 'initial',
-      }}
-    >
-      {authState.isLoading ? <div style={{alignItems:"center",textAlign:"center",marginTop:"155px",marginBottom:"155px"}}><LoadingOutlined style={{fontSize:"60px"}} /></div> : <form onSubmit={handleSubmit(onSubmit)}>
-        <Controller
-          name="username"
-          control={control}
-          defaultValue=""
-          render={({ field }) => (
-            <Form.Item
-              label="Username"
-              name="username"
-              rules={[
-                {
-                  required: true,
-                  message: 'Please input your username!',
-                },
-              ]}
-            >
-              <Input {...field}/>
-            </Form.Item>
-          )}
-        />
-        <Controller
-          name="password"
-          control={control}
-          defaultValue=""
-          render={({ field }) => (
-            <Form.Item
-              label="Password"
-              name="password"
-              rules={[
-                {
-                  required: true,
-                  message: 'Please input your password!',
-                },
-              ]}
-            >
-              <Input.Password {...field}/>
-            </Form.Item>
-          )}
-        />
+        <Row justify="center" align={"middle"}>
+        {/*<div style={{maxWidth:width/3,textAlign:"right",margin:"0 auto",position: "relative",backgroundColor:"white",padding:30,borderRadius:10,marginTop:height/5}} >*/}
+        {authState.isLoading ? <div style={{alignItems:"center",textAlign:"center",marginTop:"155px",marginBottom:"155px"}}><LoadingOutlined style={{fontSize:"60px"}} /></div> : <form onSubmit={handleSubmit(onSubmit)}>
+                <Controller
+                    name="username"
+                    control={control}
+                    defaultValue=""
+                    render={({ field }) => (
+                        <Form.Item
+                            label="Username"
+                            name="username"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input your username!',
+                                },
+                            ]}
+                        >
+                            <Input {...field}/>
+                        </Form.Item>
+                    )}
+                />
+                <Controller
+                    name="password"
+                    control={control}
+                    defaultValue=""
+                    render={({ field }) => (
+                        <Form.Item
+                            label="Password"
+                            name="password"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input your password!',
+                                },
+                            ]}
+                        >
+                            <Input.Password {...field}/>
+                        </Form.Item>
+                    )}
+                />
 
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </form>}
-    </Content>
+                <Form.Item>
+                    <Button type="primary" htmlType="submit">
+                        Login
+                    </Button>
+                </Form.Item>
+            </form>}
+        {/*</div>*/}
+        </Row>
   );
 };
 export default Login;
