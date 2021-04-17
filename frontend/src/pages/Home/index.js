8cb170bd60efb3e82895b444a40b0dc300dd9303
Umut Aydemir <umutaydemir3511@gmail.com>
2021-04-18 02:17:59 +0300

    Home Page Listing

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,14 +1,37 @@
-import React from 'react'
-import { Layout } from 'antd';
+import React, { useEffect, useContext } from 'react';
+import { Layout, PageHeader } from 'antd';
+import { ClubContext, useClubContext } from '../../context/Club';
+import ClubComponent from '../../components/ClubComponent';
 const { Content } = Layout;
 
 const Home = () => {
-	return(
-		<Content style={{ padding: '0 150px',minHeight:"100%", marginTop: 64 ,overflow: 'initial' }} >
-			<div style={{ padding: 24, minHeight: 680 }}>
-				Content
-			</div>
-		</Content>
-	)
+  const { getClubs, clubState } = useContext(ClubContext);
+
+  useEffect(() => {
+    getClubs({ name: 'tennis', category: 'sport' });
+  }, []);
+  return (
+    <div style={{ padding:25, backgroundColor: 'white !important' }}>
+      {clubState.clubs ? (
+        <PageHeader
+          className="site-page-header-responsive"
+          //onBack={() => window.history.back()}
+          title="Clubs"
+        >
+          {clubState.clubs.map((club,index) => {
+            return <ClubComponent
+                clubName={club.name}
+                clubDescription={club.description}
+                category={club.category}
+                members={club.members}
+                key={index}
+            />;
+          })}
+        </PageHeader>
+      ) : (
+        <p>No Club is available to show</p>
+      )}
+    </div>
+  );
 };
 export default Home;
