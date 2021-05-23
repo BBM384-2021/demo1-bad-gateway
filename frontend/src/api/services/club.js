49aaf1099a362281b52946fc1186edfc33e2085b
Umut Özdemir <umut.ozdemir@technarts.com>
2021-05-23 21:08:39 +0300

    conflict resolved in publicRoutes

--- frontend/src/api/services/club.js
+++ frontend/src/api/services/club.js
@@ -1,46 +1,46 @@
 import axios from 'axios';
 import {
-  API_CLUB_CREATE_URL,
-  API_CLUB_UPDATE_URL,
-  API_CLUB_DELETE_URL,
-  API_CLUB_INFO_URL,
-  API_CLUB_LIST_URL,
-  API_CLUB_SUBCLUB_LIST_URL,
-  API_CLUB_COMMENT_LIST_URL,
-  API_COMMENT_CREATE,
-  API_CLUB_ALL,
-  API_CLUB_ENROLLED,
-  API_CLUB_NAME_ALL,
-  API_CLUB_PHOTO_UPLOAD,
+    API_CLUB_CREATE_URL,
+    API_CLUB_UPDATE_URL,
+    API_CLUB_DELETE_URL,
+    API_CLUB_INFO_URL,
+    API_CLUB_LIST_URL,
+    API_CLUB_SUBCLUB_LIST_URL,
+    API_CLUB_COMMENT_LIST_URL,
+    API_COMMENT_CREATE,
+    API_CLUB_ALL,
+    API_CLUB_ENROLLED,
+    API_CLUB_NAME_ALL,
+    API_CLUB_PHOTO_UPLOAD,
 } from '../../constants/urls';
 import {getHeaderWithToken} from "../../utils/auth";
 import {apiError} from "../apiError";
 
 
 export const clubListService = (page, name, category) => {
-  return new Promise(((resolve, reject) => {
-      let query = "";
-      if(name !== null && name !== undefined && name !== ""){
-          query += `&name=${name}`;
-      }
-      if(category !== null && category !== undefined && category !== ""){
-          query += `&category=${category}`;
-      }
+    return new Promise(((resolve, reject) => {
+        let query = "";
+        if (name !== null && name !== undefined && name !== "") {
+            query += `&name=${name}`;
+        }
+        if (category !== null && category !== undefined && category !== "") {
+            query += `&category=${category}`;
+        }
 
-    axios.get(encodeURI(API_CLUB_LIST_URL + "/?page="+page+query),
-      getHeaderWithToken())
-      .then(function (response) {
-        resolve(response);
-      })
-      .catch(function (error) {
-        apiError(error, reject);
-      });
-  }));
+        axios.get(encodeURI(API_CLUB_LIST_URL + "/?page=" + page + query),
+            getHeaderWithToken())
+            .then(function (response) {
+                resolve(response);
+            })
+            .catch(function (error) {
+                apiError(error, reject);
+            });
+    }));
 };
 
 export const clubInfoService = (id, callback) => {
     return new Promise(((resolve, reject) => {
-        axios.get(encodeURI(API_CLUB_INFO_URL+"?id="+id),
+        axios.get(encodeURI(API_CLUB_INFO_URL + "?id=" + id),
             getHeaderWithToken())
             .then(function (response) {
                 resolve(response);
@@ -53,7 +53,7 @@ export const clubInfoService = (id, callback) => {
 
 export const clubCreateService = (data) => {
     return new Promise(((resolve, reject) => {
-        axios.post(encodeURI(API_CLUB_CREATE_URL),data,
+        axios.post(encodeURI(API_CLUB_CREATE_URL), data,
             getHeaderWithToken())
             .then(function (response) {
                 resolve(response);
@@ -65,7 +65,7 @@ export const clubCreateService = (data) => {
 }
 export const updateClubService = (data) => {
     return new Promise(((resolve, reject) => {
-        axios.put(encodeURI(API_CLUB_UPDATE_URL),data,
+        axios.put(encodeURI(API_CLUB_UPDATE_URL), data,
             getHeaderWithToken())
             .then(function (response) {
                 resolve(response);
@@ -78,7 +78,7 @@ export const updateClubService = (data) => {
 
 export const deleteClubService = (id) => {
     return new Promise(((resolve, reject) => {
-        axios.get(encodeURI(API_CLUB_DELETE_URL+"?id="+id),
+        axios.get(encodeURI(API_CLUB_DELETE_URL + "?id=" + id),
             getHeaderWithToken())
             .then(function (response) {
                 resolve(response);
@@ -90,55 +90,55 @@ export const deleteClubService = (id) => {
 }
 
 export const subClubListService = (clubId) => {
-  return new Promise(((resolve, reject) => {
-    axios.get(encodeURI(API_CLUB_SUBCLUB_LIST_URL+"?clubId="+clubId),
-      getHeaderWithToken())
-      .then(function (response) {
-        resolve(response);
-      })
-      .catch(function (error) {
-        apiError(error, reject);
-      });
-  }));
+    return new Promise(((resolve, reject) => {
+        axios.get(encodeURI(API_CLUB_SUBCLUB_LIST_URL + "?clubId=" + clubId),
+            getHeaderWithToken())
+            .then(function (response) {
+                resolve(response);
+            })
+            .catch(function (error) {
+                apiError(error, reject);
+            });
+    }));
 };
 
 export const clubCommentListService = (clubId) => {
-  return new Promise(((resolve, reject) => {
-    axios.get(encodeURI(API_CLUB_COMMENT_LIST_URL+"?clubId="+clubId),
-      getHeaderWithToken())
-      .then(function (response) {
-        resolve(response);
-      })
-      .catch(function (error) {
-        apiError(error, reject);
-      });
-  }));
+    return new Promise(((resolve, reject) => {
+        axios.get(encodeURI(API_CLUB_COMMENT_LIST_URL + "?clubId=" + clubId),
+            getHeaderWithToken())
+            .then(function (response) {
+                resolve(response);
+            })
+            .catch(function (error) {
+                apiError(error, reject);
+            });
+    }));
 };
 
 export const createCommentService = (data, callback) => {
-  return new Promise(((resolve, reject) => {
-    axios.post(encodeURI(API_COMMENT_CREATE), data,
-      getHeaderWithToken())
-      .then(function (response) {
-        resolve(response);
-      })
-      .catch(function (error) {
-        apiError(error, reject);
-      });
-  }));
+    return new Promise(((resolve, reject) => {
+        axios.post(encodeURI(API_COMMENT_CREATE), data,
+            getHeaderWithToken())
+            .then(function (response) {
+                resolve(response);
+            })
+            .catch(function (error) {
+                apiError(error, reject);
+            });
+    }));
 }
 
 export const commentCreateService = (data, callback) => {
-  return new Promise(((resolve, reject) => {
-    axios.post(encodeURI(API_COMMENT_CREATE), data,
-      getHeaderWithToken())
-      .then(function (response) {
-        resolve(response);
-      })
-      .catch(function (error) {
-        apiError(error, reject);
-      });
-  }));
+    return new Promise(((resolve, reject) => {
+        axios.post(encodeURI(API_COMMENT_CREATE), data,
+            getHeaderWithToken())
+            .then(function (response) {
+                resolve(response);
+            })
+            .catch(function (error) {
+                apiError(error, reject);
+            });
+    }));
 }
 
 
@@ -170,20 +170,21 @@ export const getAllClubsService = () => {
 }
 
 export const getAllClubNamesService = () => {
-  return new Promise(((resolve, reject) => {
-    axios.get(encodeURI(API_CLUB_NAME_ALL),
-      getHeaderWithToken())
-      .then(function (response) {
-        resolve(response);
-      })
-      .catch(function (error) {
-        apiError(error, reject);
-      });
-  }));
+    return new Promise(((resolve, reject) => {
+        axios.get(encodeURI(API_CLUB_NAME_ALL),
+            getHeaderWithToken())
+            .then(function (response) {
+                resolve(response);
+            })
+            .catch(function (error) {
+                apiError(error, reject);
+            });
+    }));
+}
 
 export const clubPhotoUpload = (name, data, callback) => {
     return new Promise(((resolve, reject) => {
-        axios.post(encodeURI(API_CLUB_PHOTO_UPLOAD+"?name="+name), data,
+        axios.post(encodeURI(API_CLUB_PHOTO_UPLOAD + "?name=" + name), data,
             getHeaderWithToken())
             .then(function (response) {
                 resolve(response);
